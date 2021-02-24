f1d177a2b295281bda28017987662e7c99016416
ireina7 <comcx.yang@gmail.com>
2021-02-24 21:37:10 +0800

    log

--- repo/query-web-framework/mobile/config.js
+++ repo/query-web-framework/mobile/config.js
@@ -3,13 +3,13 @@ let config = {
         title: 'Query System v0.1.7',
         footer: 'Support us? visit github@ireina7',
         theme: 'b',
-        default_query_content: 'Please load base file.',
-        prev: 'Prev',
-        next: 'Next',
-        select_msg: "It's your turn:",
-        submit: 'Submit',
-        answer: 'Show answer',
-        menu: 'Menu',
+        default_query_content: '请导入数据文件。',
+        prev: '上一题',
+        next: '下一题',
+        select_msg: "请作答：",
+        submit: '提交',
+        answer: '显示答案',
+        menu: '菜单',
         search: 'Type to search',
     },
     setting: {
@@ -18,3 +18,4 @@ let config = {
         classification: false,
     },
 };
+//# sourceMappingURL=config.js.map
